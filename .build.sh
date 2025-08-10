216449e6cb924d3487b867c8220d59542c466f44
Brett <brett.whynot@gmail.com>
2025-08-11 09:47:17 +1000

    v1.1.1

--- .build.sh
+++ .build.sh
@@ -3,7 +3,6 @@ cd "${0%/*}"
 OUTPUT="${1:-TA_perfmonmetrics.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
-chmod -R u=rwx,go= *
-python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
+chmod -R u=rwx,go= bin/*
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --overwrite TA_perfmonmetrics 
